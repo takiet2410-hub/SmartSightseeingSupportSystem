dae2bf282dceb6ec4c10ea342635cd69155d069a
Khabanh6996 <nguyentienphat061105@gmail.com>
2025-12-06 19:35:57 +0700

    Remove unnecessary files

--- DINOV2 Base/config.py
+++
@@ -1,9 +0,0 @@
-from dotenv import load_dotenv
-import os
-
-load_dotenv("config.env")
-
-MONGO_URI = os.getenv("MONGO_URI")
-DB_NAME = os.getenv("DB_NAME")
-COLLECTION_NAME = os.getenv("COLLECTION_NAME")
-DESTINATION_NAME = os.getenv("DESTINATION_NAME")
