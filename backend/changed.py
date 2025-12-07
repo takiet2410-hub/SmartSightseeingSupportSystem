a5a2d64972979986689c7aa3e2cbc292db40e35e
Phan Dinh Minh Quan <pdmquan2429@clc.fitus.edu.vn>
2025-12-07 12:30:37 +0700

    Remove changed.py containing password

--- backend/changed.py
+++
@@ -1,7 +0,0 @@
-import urllib.parse
-
-# Thay đổi dòng dưới thành mật khẩu thật của bạn
-your_password = "L@hao" 
-
-encoded_password = urllib.parse.quote_plus(your_password)
-print(encoded_password)
